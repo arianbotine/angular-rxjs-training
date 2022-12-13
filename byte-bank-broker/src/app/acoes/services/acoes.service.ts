e07380a27e72c1420cdff9172552a5f4fea3f0a4
Arian Botine <arian.botine@shift.com.br>
2022-12-12 23:26:10 -0300

    Implementando função map

--- byte-bank-broker/src/app/acoes/services/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/services/acoes.service.ts
@@ -1,13 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AcoesAPI } from '../modelo/acoes-api';
+import { map } from 'rxjs/operators'
+import { Acao } from '../modelo/acao';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AcoesService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
-  getAcoes() {
-    return this.httpClient.get<any>('http://localhost:3000/acoes');
+  public getAcoes() {
+    return this.httpClient.get<any>('http://localhost:3000/acoes')
+      .pipe(
+        map((acoes) => { return acoes.sort((acaoA, acaoB) => { this.ordenarPorCodigo(acaoA, acaoB) }) })
+      );
+  }
+
+  private ordenarPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    }
+    if (acaoA.codigo < acaoB.codigo) {
+      return -1;
+    }
+
+    return 0;
   }
 }
