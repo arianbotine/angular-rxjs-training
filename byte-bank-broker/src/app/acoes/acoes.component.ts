a5c590067e0c865f70619dc0ce16569f7a7ade5a
Arian Botine <arian.botine@shift.com.br>
2022-12-21 07:57:48 -0300

    Exemplo do uso do debouceTime

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -2,9 +2,11 @@ import { AcoesAPI } from './modelo/acoes-api';
 import { AcoesService } from './services/acoes.service';
 import { Acoes } from './modelo/acoes';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { merge, Subscription } from 'rxjs';
-import { filter, switchMap, tap } from 'rxjs/operators';
+import { debounceTime, filter, switchMap, tap } from 'rxjs/operators';
+
+const ESPERA_DIGITACAO = 300;
 
 @Component({
   selector: 'app-acoes',
@@ -13,16 +15,17 @@ import { filter, switchMap, tap } from 'rxjs/operators';
 })
 export class AcoesComponent {
   acoesInput = new FormControl();
-  todasAcoes$ = this.acoesService.getAcoes().pipe(tap(() => { console.log('Fluxo inicial') }));
+  todasAcoes$ = this.acoesService.getAcoes();
   acoesFiltradas$ = this.acoesInput.valueChanges
     .pipe(
       filter((valorDigitado) => {
         return valorDigitado.length >= 3 || !valorDigitado.length
       }),
+      debounceTime(ESPERA_DIGITACAO),
+      tap((valorDigitado) => console.log('Fluxo filtrado ' + valorDigitado)),
       switchMap((valorDigitado) => {
         return this.acoesService.getAcoes(valorDigitado)
-      }),
-      tap(() => console.log('Fluxo filtrado')));
+      }));
 
   acoes$ = merge(this.todasAcoes$, this.acoesFiltradas$);
 
